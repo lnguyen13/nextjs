0b1abbd6bed2e8b872159e2c13888cefad850444
luan-minh-nguyen <luan.min.nguyen@gmail.com>
2021-01-28 10:48:49 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,8 @@ export default function Home() {
         <h1 className={styles.title}>
           YES! 🚀
         </h1>
-    <p class="description">You like the stock. Why wouldn't you?</p>
-    <br/>
-    <a href="https://shouldibuygme.com">Next Step</a><
+    <p class="description">You like the stock. Why wouldn't you? <br/> <a href="https://shouldibuygme.com">Next Step</a></p>
+    
       </main>
     <footer>The contents on this site are for informational and entertainment purposes only and does not constitute financial, accounting, or legal advice.</footer>
     </div>
